d20f53d27d7ed0e7026217148b6c2f0fd694dc4e
Mohamed Idrissi <midrissi@w-r6-p1.s19.be>
2019-04-01 20:26:24 +0200

    fils de pute

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 15:31:17 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/07 01:19:52 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/04/01 20:19:30 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,25 @@
 # include <fcntl.h>
 # include <dirent.h>
 # include <time.h>
-# define LSFLAGS            "lRarftug"
-# define F_LIST             (1 << 0)
-# define F_RECURSIVE        (1 << 1)
-# define F_DOT              (1 << 2)
-# define F_REVERSE          (1 << 3)
+# define LSFLAGS			"lRarftug"
+# define F_LIST				(1 << 0)
+# define F_RECURSIVE		(1 << 1)
+# define F_DOT				(1 << 2)
+# define F_REVERSE			(1 << 3)
 # define F_SORT_TIME        (1 << 4)
-# define F_LAST_ACCESS_TIME (1 << 5)
-# define F_SORT_OFF         (1 << 6)
-# define F_COLOR            (1 << 7)
+# define F_LAST_ACCESS_TIME	(1 << 5)
+# define F_SORT_OFF			(1 << 6)
+# define F_COLOR			(1 << 7)
+# define ANSI_RED			"\x1b[31m"
+# define ANSI_GREEN			"\x1b[1m\x1b[32m"
+# define ANSI_YELLOW		"\x1b[33m"
+# define ANSI_BLUE			"\x1b[1m\x1b[34m"
+# define ANSI_MAGENTA		"\x1b[1m\x1b[35m"
+# define ANSI_CYAN			"\x1b[1m\x1b[36m"
+# define ANSI_RESET			"\x1b[0m"
+# define ANSI_PURPLE		"\x1b[35m"
+# define ISEXEC(m)			(((m) & S_IXUSR) == S_IXUSR)
+# define MAX_PATH_LEN		4096
 
 t_list						*g_files;
 extern char					g_flags;
@@ -44,9 +54,10 @@ typedef struct group		t_group;
 typedef struct				s_file
 {
 	t_stat					stats;
+	size_t						size;
 	char					perms[11];
 	char					*name;
-	char					full_path[PATH_MAX];
+	char					full_path[MAX_PATH_LEN];
 }							t_file;
 
 #endif
--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 15:30:46 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/28 15:25:05 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/04/01 20:26:07 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,10 @@
 
 char g_flags = 0;
 
-void		list_dir(DIR *dir)
+char		get_extended(t_file file)
 {
-	t_dirent	*d;
-	size_t		size;
-
-	size = 10;
-	while ((d = readdir(dir)))
-	{
-		if (size < ft_strlen(d->d_name))
-			size = ft_strlen(d->d_name) + 1;
-		if (*(d->d_name) != '.' || (g_flags & F_DOT))
-			ft_printf("%-*s", size, d->d_name);
-	}
-	ft_printf("\n");
-	closedir(dir);
+	(void)file;
+	return ('@');
 }
 
 char		third_permission(int m, char type_user)
@@ -41,17 +30,20 @@ char		third_permission(int m, char type_user)
 		return ((char[2][2]){"-T", "xt"}[(m & S_IXOTH) > 0][(m & S_ISTXT) > 0]);
 }
 
-char		get_extended(t_file file)
+static void		cat_fullpath(t_file *file, char *name, char *path)
 {
-	(void)file;
-	return ('@');
+	ft_bzero(file->full_path, MAX_PATH_LEN);
+	ft_strcpy(file->full_path, path);
+	ft_strcat(file->full_path, "/");
+	ft_strcat(file->full_path, name);
 }
 
-t_file		create_file(char *name)
+t_file			create_file(char *name, char *path)
 {
 	t_file		file;
 
-	stat(name, &(file.stats));
+	cat_fullpath(&file, name, path);
+	lstat(file.full_path, &(file.stats));
 	file.name = name;
 	file.perms[0] = 0;
 	(S_ISREG(file.stats.st_mode)) && (file.perms[0] = '-');
@@ -62,36 +54,87 @@ t_file		create_file(char *name)
 	(S_ISLNK(file.stats.st_mode)) && (file.perms[0] = 'l');
 	(S_ISSOCK(file.stats.st_mode)) && (file.perms[0] = 's');
 	file.perms[0] || (file.perms[0] = '?');
-
 	file.perms[1] = "-r"[(file.stats.st_mode & S_IRUSR) > 0];
 	file.perms[2] = "-w"[(file.stats.st_mode & S_IWUSR) > 0];
 	file.perms[3] = third_permission(file.stats.st_mode, 'u');
-
 	file.perms[4] = "-r"[(file.stats.st_mode & S_IRGRP) > 0];
 	file.perms[5] = "-w"[(file.stats.st_mode & S_IWGRP) > 0];
 	file.perms[6] = third_permission(file.stats.st_mode, 'g');
-
 	file.perms[7] = "-r"[(file.stats.st_mode & S_IROTH) > 0];
 	file.perms[8] = "-w"[(file.stats.st_mode & S_IWOTH) > 0];
 	file.perms[9] = third_permission(file.stats.st_mode, 'o');
-
 	file.perms[10] = get_extended(file);
 	return (file);
 }
 
-void		print_full_info(t_file file)
+void		list_dir(DIR *dir, t_list **begin, char *path)
 {
-	ft_printf("%s %ld %s %s %lld %.12s %s\n",
-		file.perms,
-		file.stats.st_nlink,
-		getpwuid(file.stats.st_uid)->pw_name,
-		getgrgid(file.stats.st_gid)->gr_name,
-		file.stats.st_size,
-		ctime(&file.stats.st_mtimespec.tv_sec) + 4,
-		file.name);
+	t_dirent	*d;
+	t_list		*list;
+	t_file		file;
+
+	list = NULL;
+	while ((d = readdir(dir)))
+	{
+		file = create_file(d->d_name, path);
+		file.size = ft_strlen(d->d_name);
+		file.name = d->d_name;
+		list = ft_lstnew((void *)&file, sizeof(t_file));
+		list == NULL ? exit(1) : 0;
+		ft_lstadd(begin, list);
+	}
+	closedir(dir);
 }
 
-void		print_flags(void)
+static void		print_full_info(t_list *files)
+{
+	t_file	file;
+
+	while (files)
+	{
+		file = *((t_file *)files->content);
+		ft_printf("%s %ld %s %s %lld %.12s %s\n",
+			file.perms,
+			file.stats.st_nlink,
+			getpwuid(file.stats.st_uid)->pw_name,
+			getgrgid(file.stats.st_gid)->gr_name,
+			file.stats.st_size,
+			ctime(&file.stats.st_mtimespec.tv_sec) + 4,
+			file.name);
+		files = files->next;
+	}
+}
+
+static void		simple_print(t_list *files)
+{
+	size_t	size;
+	t_file	file;
+
+	size = 10;
+	while (files)
+	{
+		file = *((t_file*)files->content);
+		if (size < file.size)
+			size = file.size + 1;
+		if (*(file.name) != '.' || (g_flags & F_DOT))
+		{
+			if (S_ISDIR(file.stats.st_mode))
+				ft_printf(ANSI_CYAN "%-*s" ANSI_RESET, size, file.name);
+			else if (S_ISFIFO(file.stats.st_mode))
+				ft_printf(ANSI_YELLOW "%-*s" ANSI_RESET, size, file.name);
+			else if (S_ISLNK(file.stats.st_mode))
+				ft_printf(ANSI_PURPLE "%-*s" ANSI_RESET, size, file.name);
+			else if ((((file.stats.st_mode) & S_IXUSR) == S_IXUSR))
+				ft_printf(ANSI_RED "%-*s" ANSI_RESET, size, file.name);
+			else
+				ft_printf("%-*s", size, file.name);
+		}
+		files = files->next;
+	}
+	ft_printf("\n");
+}
+
+void			print_flags(void)
 {
 	(g_flags & F_LIST) && (ft_printf("g_flags: F_LIST\n"));
 	(g_flags & F_RECURSIVE) && (ft_printf("g_flags: F_RECURSIVE\n"));
@@ -103,11 +146,12 @@ void		print_flags(void)
 	(g_flags & F_COLOR) && (ft_printf("g_flags: F_COLOR\n"));
 }
 
-void		set_lsflags(int argc, char **argv)
+static void			set_lsflags(int argc, char **argv)
 {
 	int i;
 
 	i = -1;
+	g_flags = 0;
 	while (++i < argc)
 		if (*argv[i] == '-' && argv[i]++)
 			while (*(argv[i]) && *(argv[i]) != ' ')
@@ -118,18 +162,39 @@ void		set_lsflags(int argc, char **argv)
 			}
 }
 
-int			main(int argc, char **argv)
+static	void		ft_ls(char *name)
 {
 	DIR			*dir;
-	t_file		file;
+	t_list		*files;
+	char		*err;
 
-	if (argc < 2)
-		return (0);
+	files = NULL;
+	dir = opendir(name);
+	if (!dir)
+	{
+		err = ft_strjoin("ls: ", name);
+		perror(err);
+		ft_strdel(&err);
+		return ;
+	}
+	list_dir(dir, &files, name);
+	if (!g_flags || (~g_flags & F_LIST))
+		simple_print(files);
+	else
+		print_full_info(files);
+}
+
+int					main(int argc, char **argv)
+{
+	int			i;
+
 	set_lsflags(argc, argv);
-	print_flags();
-	dir = opendir(argv[2]);
-	list_dir(dir);
-	file = create_file(argv[2]);
-	print_full_info(file);
+	(g_flags > 0) && (argv++);
+	i = 1;
+	while (i < (argc - (g_flags > 0)))
+		ft_ls(argv[i++]);
+	if (i == 1)
+		ft_ls(".");
+	// print_flags();
 	return (0);
 }
