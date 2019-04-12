cc3b7929b63a60907ca72b02d12c6cd2d52dcc4f
Mohamed Idrissi <skenz@Mohameds-iMac.local>
2019-04-12 06:00:49 +0200

    l;dalmaw

--- srcs/utils2.c
+++ srcs/utils2.c
@@ -6,38 +6,13 @@
 /*   By: Mohamed <Mohamed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 11:14:51 by Mohamed           #+#    #+#             */
-/*   Updated: 2019/04/12 05:45:39 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/04/12 05:59:16 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
 int			sort_args(int argc, char **argv, char reverse)
-{
-	int		sorted;
-	int		j;
-	char	*temp;
-	int		ret;
-
-	sorted = 0;
-	while (!sorted)
-	{
-		j = -1;
-		sorted = 1;
-		while (++j < argc)
-			if (argv[j + 1] && (ret = ft_strcmp(argv[j], argv[j + 1])))
-				if ((!reverse && ret > 0) || (reverse && ret < 0))
-				{
-					sorted = 0;
-					temp = argv[j];
-					argv[j] = argv[j + 1];
-					argv[j + 1] = temp;
-				}
-	}
-	return (1);
-}
-
-int			sort_fiflnks(int argc, char **argv, char reverse)
 {
 	int		sorted;
 	int		j;
